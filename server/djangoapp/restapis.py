51f2afcee5f4bf4889751d9d1e5ad42592b431ed
berketonoz <btonoz@sabanciuniv.edu>
2024-05-06 09:03:44 -0700

    restapi endpoint implemented for post method

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -9,7 +9,7 @@ backend_url = os.getenv(
     'backend_url', default="http://localhost:3030")
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
-    default="http://localhost:5000")
+    default="http://127.0.0.1:5000")
 
 def get_request(endpoint, **kwargs):
     params = ""
@@ -32,7 +32,7 @@ def get_request(endpoint, **kwargs):
 # request_url = sentiment_analyzer_url+"analyze/"+text
 # Add code for retrieving sentiments
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"analyze/"+text
+    request_url = sentiment_analyzer_url+"/analyze/"+text
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
@@ -43,3 +43,11 @@ def analyze_review_sentiments(text):
 
 # def post_review(data_dict):
 # Add code for posting review
+def post_review(data_dict):
+    request_url = backend_url+"/insert_review"
+    try:
+        response = requests.post(request_url,json=data_dict)
+        print(response.json())
+        return response.json()
+    except:
+        print("Network exception occurred")
