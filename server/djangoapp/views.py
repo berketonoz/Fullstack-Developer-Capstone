cb45051ee898394836ea6a36f7facc3d763bc5cb
berketonoz <btonoz@sabanciuniv.edu>
2024-05-06 03:01:40 -0700

    get_dealer_details view added

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -94,16 +94,22 @@ def get_dealerships(request, state="All"):
         endpoint = "/fetchDealers"
     else:
         endpoint = "/fetchDealers/"+state
-    # dealerships = get_request(endpoint)
-    return JsonResponse({"status":200}) # ,"dealers":dealerships})
+    dealerships = get_request(endpoint)
+    return JsonResponse({"status":200,"dealers":dealerships})
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 # def get_dealer_reviews(request,dealer_id):
 # ...
 
+
 # Create a `get_dealer_details` view to render the dealer details
 # def get_dealer_details(request, dealer_id):
 # ...
+def get_dealer_details(request,dealer_id):
+    endpoint = '/fetchDealer/'+dealer_id
+    dealer = get_request(endpoint)
+    print(dealer)
+    return JsonResponse({"status":200, "dealer": dealer})
 
 # Create a `add_review` view to submit a review
 # def add_review(request):
