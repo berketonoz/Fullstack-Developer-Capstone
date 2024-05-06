b5726593e881a8bce42801b32d93983727b436ff
berketonoz <btonoz@sabanciuniv.edu>
2024-05-06 03:02:36 -0700

    get_dealer_details route path added

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -21,6 +21,12 @@ urlpatterns = [
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
 
+    # path for get_dealer_details
+    path(route='get_dealer_details', view=views.get_dealer_details, name='get_dealer_details'),
+
+    # path for get_dealer_reviews
+    path(route='get_dealer_reviews', view=views.get_dealer_reviews, name='get_dealer_reviews'),
+
     # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
