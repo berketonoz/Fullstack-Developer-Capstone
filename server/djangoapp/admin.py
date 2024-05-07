8256043cedbdfd44fc2e3eccd50e34be9a56b535
berketonoz <btonoz@sabanciuniv.edu>
2024-05-07 05:15:00 -0700

    admin site implemented

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,3 +1,4 @@
+"""admin.py import statements"""
 from django.contrib import admin
 from .models import CarMake, CarModel
 
@@ -5,10 +6,20 @@ from .models import CarMake, CarModel
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    """CarModelInline Admin StackedInline Class"""
+    model = CarMake
+    inline  = [2]
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    """CarModelAdmin Admin Model Class"""
+    fields = ['name', 'type', 'year', 'mileage', 'car_make']
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    """CarMakeAdmin Admin Model Class"""
+    fields = ['name', 'description']
 
 # Register models here
 admin.site.register(CarMake)
