253e5acc381a0bd68f781bf2c6d58b66a078be33
berketonoz <btonoz@sabanciuniv.edu>
2024-05-07 06:53:05 -0700

    lint fixes v2:urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -25,24 +25,17 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name="Home.html")),
 
     # About route
-    path('about/',
-        TemplateView.as_view(template_name="About.html")),
+    path('about/', TemplateView.as_view(template_name="About.html")),
     # Contact route
-    path('contact/',
-        TemplateView.as_view(template_name="Contact.html")),
+    path('contact/', TemplateView.as_view(template_name="Contact.html")),
     # Login route
-    path('login/',
-        TemplateView.as_view(template_name="index.html")),
+    path('login/', TemplateView.as_view(template_name="index.html")),
     # Register route
-    path('register/',
-        TemplateView.as_view(template_name="index.html")),
+    path('register/', TemplateView.as_view(template_name="index.html")),
     # Dealers route
-    path('dealers/',
-        TemplateView.as_view(template_name="index.html")),
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
     # Dealer route
-    path('dealer/<int:dealer_id>',
-        TemplateView.as_view(template_name="index.html")),
+    path('dealer/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
     # Post Review route
-    path('postreview/<int:dealer_id>',
-        TemplateView.as_view(template_name="index.html")),
+    path('postreview/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
